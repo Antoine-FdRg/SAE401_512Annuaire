9db3a66e84ec8dc826e8a44ecbdd1d244972a416
fa103555 <antoine.fadda-rodriguez@etu.univ-cotedazur.fr>
2023-02-02 11:10:05 +0100

    :test_tube: Code de test #2 de la connexion #10

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,20 @@ import javax.naming.NamingException;
 import javax.naming.directory.DirContext;
 import javax.naming.directory.InitialDirContext;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         String user = "uid=arnaud,ou=system";
         String mdp = "@Arnaudisthebest83";
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("entrer le port :");
+        String port = sc.nextLine();
+
         Properties env = new Properties();
         env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-        env.put(Context.PROVIDER_URL, "ldap://localhost:10389");
+        env.put(Context.PROVIDER_URL, "ldap://localhost"+port);
         env.put(Context.SECURITY_PRINCIPAL, user);
         env.put(Context.SECURITY_CREDENTIALS,mdp);
         try {
