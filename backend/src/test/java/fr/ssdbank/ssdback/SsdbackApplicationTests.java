c9d30866056fa68af21db6d0da477b2c2e7c58d7
Raphaël C <raphael.caldwell@etu.univ-cotedazur.fr>
2023-02-06 14:03:01 +0100

    Ajouter le code de spring boot

---
+++ backend/src/test/java/fr/ssdbank/ssdback/SsdbackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ssdbank.ssdback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsdbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
