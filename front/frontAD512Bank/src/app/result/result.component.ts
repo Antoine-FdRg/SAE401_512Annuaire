9395b95f2618f82be1f8caac4728273c811c8eff
ma102741 <antoine.maistre@etu.unice.fr>
2023-02-06 17:26:27 +0100

    debut d'implémentation des fiches de personnes

--- front/frontAD512Bank/src/app/result/result.component.ts
+++ front/frontAD512Bank/src/app/result/result.component.ts
@@ -8,16 +8,33 @@ import { Person } from '../person';
 export class ResultComponent {
   listPerson: Person[] = [];
 
-  constructor(){
-    this.listPerson.push({
-      surname:'Maïstre',
-      name:'Antoine',
-      phone:'+33 95 74 24 12',
-      email:'truc@machin.com',
-      role:'Directeur financier',
-      login:'ma102741',
-      tutor:'raph@free.fr'
-    });
+  constructor() {
+    this.listPerson.push(
+      {
+        surname: 'Maïstre',
+        name: 'Antoine',
+        phone: '+33 95 74 24 12',
+        email: 'truc@machin.com',
+        role: 'Directeur financier',
+        login: 'ma102741',
+        tutor: 'raph@free.fr'
+      },
+      {
+        surname: 'Caldwell',
+        name: 'Raphaël',
+        phone: '+33 95 74 24 12',
+        email: 'raphelCon@raphael.con',
+        role: 'Con',
+        login: 'con102741',
+        tutor: 'antoine.maistre@etu.unice.fr'
+      }
+
+
+    );
+
+
   }
+
+
 
 }
