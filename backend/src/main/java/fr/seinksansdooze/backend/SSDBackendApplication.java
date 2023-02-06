f0158aefdc47286d84113808f50e49e9d4035702
Raphaël C <raphael.caldwell@etu.univ-cotedazur.fr>
2023-02-06 14:43:46 +0100

    [BACK] Petits changements

--- backend/src/main/java/fr/seinksansdooze/backend/SSDBackendApplication.java
+++ backend/src/main/java/fr/seinksansdooze/backend/SSDBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SSDBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SSDBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SSDBackendApplication.class, args);
+    }
 
 }
