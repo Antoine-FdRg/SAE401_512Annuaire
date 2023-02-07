6a7eabfef5ff8f0a98824300dca14c8d02e240bb
lc101219 <clement.lefevre1@etu.univ-cotedazur.fr>
2023-02-07 13:55:25 +0100

    [FRONT] Implémentation du changement personnel -> structure

--- front/frontAD512Bank/src/app/home-page/home-page.component.ts
+++ front/frontAD512Bank/src/app/home-page/home-page.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class HomePageComponent {
 
+  constructor() {
+
+   }
+
+  openDropdown() {
+    document.getElementsByClassName("dropdown")![0].classList.toggle("show");
+  }
+  selectHouse() {
+    document.getElementById("house")!.style.display="block";
+    document.getElementById("person")!.style.display="none";
+    document.getElementById("house1")!.style.display="none";
+    document.getElementById("person1")!.style.display="block";
+  }
+  selectPerson() {
+    document.getElementById("person")!.style.display="block";
+    document.getElementById("house")!.style.display="none";
+    document.getElementById("person1")!.style.display="none";
+    document.getElementById("house1")!.style.display="block";
+  }
+
 }
