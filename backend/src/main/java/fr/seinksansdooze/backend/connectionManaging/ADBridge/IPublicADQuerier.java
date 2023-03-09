07773fefb5a4246dee386060ddb9a9b6f3de3f39
fa103555 <antoine.fadda-rodriguez@etu.univ-cotedazur.fr>
2023-03-09 11:35:02 +0100

    [BACK] Ajout de la query AD pour chercher les membres d'un groupe et ajout de la méthode de searchPerson définitive #30

--- backend/src/main/java/fr/seinksansdooze/backend/connectionManaging/ADBridge/IPublicADQuerier.java
+++ backend/src/main/java/fr/seinksansdooze/backend/connectionManaging/ADBridge/IPublicADQuerier.java
@@ -1,13 +1,16 @@
 package fr.seinksansdooze.backend.connectionManaging.ADBridge;
 
+import fr.seinksansdooze.backend.model.response.PartialPerson;
+
 import javax.naming.NamingEnumeration;
 import javax.naming.directory.SearchResult;
+import java.util.ArrayList;
 
 public interface IPublicADQuerier {
 
     NamingEnumeration<SearchResult> search(ObjectType searchType, String searchValue);
 
-    NamingEnumeration<SearchResult> searchPerson(String cn);
+    ArrayList<PartialPerson> searchPerson(String cn);
 
     NamingEnumeration<SearchResult> searchStructure(String ou);
 }
