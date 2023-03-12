85184c0f309dfaf4fa7400bb70a73e057ac1d899
Itskatt <raph10@mail.com>
2023-03-12 14:50:59 +0100

    [BACK] Adaptation du login/logout avec le connectionManager #42

--- backend/src/main/java/fr/seinksansdooze/backend/controller/UserController.java
+++ backend/src/main/java/fr/seinksansdooze/backend/controller/UserController.java
@@ -1,7 +1,13 @@
 package fr.seinksansdooze.backend.controller;
 
+import fr.seinksansdooze.backend.connectionManaging.ADBridge.IAdminADQuerier;
+import fr.seinksansdooze.backend.connectionManaging.ADConnectionManager;
+import fr.seinksansdooze.backend.connectionManaging.TokenGenerator;
+import fr.seinksansdooze.backend.connectionManaging.TokenSanitizer;
+import fr.seinksansdooze.backend.model.SeinkSansDoozeBackException;
 import fr.seinksansdooze.backend.model.payload.LoginPayload;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,12 +15,29 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.naming.NamingException;
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+    ADConnectionManager connectionManager = new ADConnectionManager(
+            new TokenGenerator(),
+            new TokenSanitizer(),
+            IAdminADQuerier.class
+    );
+
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginPayload payload) {
-        // TODO: 10/03/2023 verification mdp
+        String token;
+
+        try {
+            token = connectionManager.addConnection(payload);
+        } catch (NamingException e) {
+            throw new SeinkSansDoozeBackException(
+                    HttpStatus.UNAUTHORIZED,
+                    "La connexion à échouée. Veuillez verifier votre identifiant ainsi que votre mot de passe."
+            );
+        }
 
         // Durée de vie de la session
         int sessionAge;
@@ -24,9 +47,9 @@ public class UserController {
             sessionAge = 60 * 60; // 1 heure
         }
 
-        ResponseCookie responseCookie = ResponseCookie.from("token", "TODO")
+        ResponseCookie responseCookie = ResponseCookie.from("token", token)
                 .httpOnly(true)
-                //.secure(false)
+                //.secure(false) // a voir ce qu'on fait (le cookie est seulement transmis par https
                 .path("/")
                 .maxAge(sessionAge)
                 .build();
@@ -38,6 +61,8 @@ public class UserController {
 
     @PostMapping("/logout")
     public ResponseEntity<String> logout() {
+//        TODO connectionManager.logout...
+
         ResponseCookie resCookie = ResponseCookie.from("token", "")
                 .httpOnly(true)
                 .path("/")
