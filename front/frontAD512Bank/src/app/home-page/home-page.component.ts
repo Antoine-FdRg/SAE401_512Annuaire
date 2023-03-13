2bdb21a7ecb55d44247c6c9209f451c993ce97dd
ma102741 <antoine.maistre@etu.unice.fr>
2023-03-13 15:41:19 +0100

    avancement de l'interface du menu de profil

--- front/frontAD512Bank/src/app/home-page/home-page.component.ts
+++ front/frontAD512Bank/src/app/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ApplicationRef, Component, ElementRef, Injector, ViewChild } from '@angular/core';
 import { elementAt } from 'rxjs';
 
 @Component({
@@ -10,6 +10,16 @@ export class HomePageComponent {
   branches = ["tout", "financier", "commerce", "développement", "etc"];
   catégories = [];
 
+
+  // todo : déclarer un service pour setup les variables de l'utilisateur à afficher (nom, initiales)
+
+  // appElementRef: ElementRef;
+  // constructor(private applicationRef:ApplicationRef, injector: Injector) {
+  //   this.appElementRef = injector.get(applicationRef.componentTypes[0]).elementRef;
+
+  //   let rootElement : HTMLElement = this.appElementRef.nativeElement as HTMLElement;
+  //   rootElement.style.setProperty('--initials', initials);
+  // }
 
 
   public onSearch(event : Event){
--- front/frontAD512Bank/src/app/home-page/profile/profile.component.ts
+++ front/frontAD512Bank/src/app/home-page/profile/profile.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild, ElementRef, HostListener, ApplicationRef, Injector } from '@angular/core';
+
+enum Status {
+  connected,
+  diconnected
+}
 
 @Component({
   selector: 'app-profile',
@@ -6,14 +11,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
-  status = ""
-  imageLink = ""
-  name = "jean"
-  surname= "paul"
+  status = Status.diconnected
+  menuShown = false;
+
+  imageLink = "";
+  name = "Jean";
+  surname = "Paul";
+  letters : string = this.name.charAt(0) + this.surname.charAt(0);
 
   visibility = {
-    "profilePicture" : false,
-    "connexion" : true,
-    "infoMenu" : false
+    "profilePicture": false,
+    "connexion": true,
+    "infoMenu": false
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    let target : HTMLElement = event.target as HTMLElement;
+    if(target.id !== "connexionButton"){
+      this.visibility["infoMenu"] = false;
+    }
+  }
+
+  connexionClicked(){
+    if(this.menuShown){
+      this.visibility["infoMenu"] = false;
+      this.menuShown = false;
+      return;
+    }
+    this.menuShown = true;
+    this.visibility["infoMenu"] = true;
+
   }
 }
+
+
