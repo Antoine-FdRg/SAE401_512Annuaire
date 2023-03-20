de44f3653b2834c4b8bfc0bfd01b8c0db765a183
Raphaël C <raphael.caldwell@etu.univ-cotedazur.fr>
2023-03-20 15:12:23 +0100

    [BACK] Temporairement désactiver les tests spring en attendant de mock la connexion à AD

--- backend/src/test/java/fr/seinksansdooze/backend/SSDBackendApplicationTests.java
+++ backend/src/test/java/fr/seinksansdooze/backend/SSDBackendApplicationTests.java
@@ -1,9 +1,11 @@
 package fr.seinksansdooze.backend;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled // FIXME: 20/03/2023 Temporaire en attendant de mock la connexion à AD
 class SSDBackendApplicationTests {
 
     @Test
