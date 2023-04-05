4a947581ac0d60c477ac3e3aff4ad5f6fcf10201
fa103555 <antoine.fadda-rodriguez@etu.univ-cotedazur.fr>
2023-04-05 18:47:51 +0200

    [BACK] Corrections post refill de l'AD

--- backend/src/main/java/fr/seinksansdooze/backend/connectionManaging/ADBridge/TestADQuerier.java
+++ backend/src/main/java/fr/seinksansdooze/backend/connectionManaging/ADBridge/TestADQuerier.java
@@ -5,75 +5,19 @@ import fr.seinksansdooze.backend.connectionManaging.ADBridge.interfaces.IPublicA
 public class TestADQuerier {
 
     public static void main(String[] args) {
-        testPublicQuerier();
+//        testPublicQuerier();
         System.out.println("====================================");
         testAuthenticatedQuerier();
-
-//
-
-//        System.out.println("api/info/person/{cn}");
-//        System.out.println(adQuerier.getPersonInfo("Thomas TG. Gorisse"));
-
-//        System.out.println("api/info/structure/{ou}");
-//        System.out.println(adQuerier.getStructureInfo("OU=Direction Général,OU=512Direction,OU=512Batiment,OU=512BankFR,DC=EQUIPE1B,DC=local"));
-
-//        System.out.println("api/admin/group/all");
-//        results = adQuerier.searchAllGroups();
-//        displayResults(results, ObjectType.GROUP);
-
-//        System.out.println("api/admin/group/create/{groupName}");
-//        if (adQuerier.createGroup("test")) {
-//            System.out.println("Group created");
-//        } else {
-//            System.out.println("Group not created");
-//        }
-//
-//        System.out.println("api/admin/group/delete/{groupName}");
-//        if(adQuerier.deleteGroup("test")){
-//            System.out.println("Group deleted");
-//        } else {
-//            System.out.println("Group not deleted");
-//        }
-
-//        System.out.println("api/admin/group/add/{groupName}/{cn}");
-//        if (adQuerier.addUserToGroup("test", "Clément CL. Lefèvre")) {
-//            System.out.println("User added to group");
-//        } else {
-//            System.out.println("User not added to group");
-//        }
-
-//        System.out.println("api/admin/group/remove/{groupName}/{username}");
-//        if(adQuerier.removeUserFromGroup("test", "Thomas TG. Gorisse")){
-//            System.out.println("User removed from group");
-//        } else {
-//            System.out.println("User not removed from group");
-//        }
-
-//        System.out.println("api/admin/search/person/{person}/{groupName}");
-//        results = adQuerier.searchPerson("Thomas TG. Gorisse", "test");
-//        displayResults(results, ObjectType.GROUP);
-
-        //api/admin/group/members/{groupName}
-//        System.out.println("api/admin/group/members/{groupName}");
-//        ArrayList<SearchResult> ALresults = adQuerier.searchGroupMembers("test");
-//        displayResults(ALresults, ObjectType.GROUP);
-
-//        System.out.println("CN=Clément CL. Lefèvre,OU=Direction Général Adj,OU=512Direction,OU=512Batiment,OU=512BankFR,DC=EQUIPE1B,DC=local".split(",")[0].split("=")[1]);
-
-//          adQuerier.searchPerson("clément").forEach(System.out::println);
-//          adQuerier.searchStructure("Direction").forEach(System.out::println);
-
-//            adQuerier.getAllGroups().forEach(System.out::println);
     }
 
     private static void testAuthenticatedQuerier() {
-        String id = "antoine.fadda.rodriguez";
+        String id = "fadda.antoine";
         String pwd = "@Arnaudisthebest83";
 
         IAuthentifiedADQuerier querier = new AuthentifiedADQuerier(id, pwd);
 //        querier.getAllGroups().forEach(System.out::println);
-//        System.out.println("Récupération des infos de Thomas");
-//        System.out.println(querier.getFullPersonInfo("Thomas TG. Gorisse"));
+        System.out.println("Récupération des infos de Thomas");
+        System.out.println(querier.getFullPersonInfo("Gorisse Thomas"));
 //        System.out.println("Ajout de Thomas au groupe test");
 //        System.out.println(querier.addUserToGroup( "Thomas TG. Gorisse", "test"));
 //        System.out.println("Affichage des membres du groupe test");
@@ -83,7 +27,7 @@ public class TestADQuerier {
 //        System.out.println("Affichage des membres du groupe test");
 //        querier.getGroupMembers("test").forEach(System.out::println);
 //        System.out.println("Récupération des infos de la structure Direction Générale");
-        querier.searchPerson("Antoine","postalCode","06000",0,15).forEach(System.out::println); //cn, initials, sn, givenName, postalCode, (postalAdress), (manager)
+//        querier.searchPerson("Antoine","postalCode","06000",0,15).forEach(System.out::println); //cn, initials, sn, givenName, postalCode, (postalAdress), (manager)
 
         querier.logout();
 
--- backend/src/main/java/fr/seinksansdooze/backend/controller/MemberController.java
+++ backend/src/main/java/fr/seinksansdooze/backend/controller/MemberController.java
@@ -5,6 +5,7 @@ import fr.seinksansdooze.backend.connectionManaging.ADConnectionManagerSingleton
 import fr.seinksansdooze.backend.connectionManaging.rateLimit.RateLimiterSingleton;
 import fr.seinksansdooze.backend.connectionManaging.tokenManaging.TokenSanitizer;
 import fr.seinksansdooze.backend.model.exception.SeinkSansDoozeBackException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,7 +22,7 @@ public class MemberController {
     ADConnectionManager connectionManager = ADConnectionManagerSingleton.INSTANCE.get();
 
     @PostMapping("/changePassword")
-    public ResponseEntity changePassword(@CookieValue("token") String token, @RequestBody String cn, @RequestBody String oldPassword, @RequestBody String newPassword, ServerHttpRequest request){
+    public ResponseEntity changePassword(@RequestHeader(HttpHeaders.AUTHORIZATION) String token, @RequestBody String cn, @RequestBody String oldPassword, @RequestBody String newPassword, ServerHttpRequest request){
         RateLimiterSingleton.get().tryConsume(String.valueOf(request.getLocalAddress()));
 
         if(!new TokenSanitizer().valideToken(token)){
