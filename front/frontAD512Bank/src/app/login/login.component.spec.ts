d93498e1faa2ddda22bd986a9869040cbab8daf2
Clement LEFEVRE <clement.lefevre1@etu.univ-cotedazur.fr>
2023-04-05 18:25:26 +0200

    [FRONT] Correction des fichiers de tests Angular

--- front/frontAD512Bank/src/app/login/login.component.spec.ts
+++ front/frontAD512Bank/src/app/login/login.component.spec.ts
@@ -1,16 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      imports: [HttpClientTestingModule],
+
+      declarations: [LoginComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
