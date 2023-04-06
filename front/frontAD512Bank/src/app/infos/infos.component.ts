27302e6b63eb3221c5b17e27df90884028bd4e8a
ma102741 <antoine.maistre@etu.unice.fr>
2023-04-06 11:16:46 +0200

    [FRONT] avancement du front des informations personelles

--- front/frontAD512Bank/src/app/infos/infos.component.ts
+++ front/frontAD512Bank/src/app/infos/infos.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../service/login.service';
+import { Person } from '../person';
 
 @Component({
   selector: 'app-infos',
   templateUrl: './infos.component.html',
   styleUrls: ['./infos.component.css']
 })
-export class InfosComponent {
 
+export class InfosComponent {
+  modified = {
+    "address" : false,
+    "name"    : false,
+    "mail"    : false,
+    "surname" : false,
+    "phone"   : false,
+  };
+
+  ChangingField = ChangingField;
+  user : Person | undefined;
+
+  constructor(public loginService : LoginService){
+    this.user = loginService.getUser();
+  }
+
+
+  newField(field : ChangingField){
+    switch (field){
+      case ChangingField.address :
+        break;
+    }
+  }
+  
+}
+
+enum ChangingField {
+  surname,
+  name,
+  address,
+  phone,
+  mail
 }
