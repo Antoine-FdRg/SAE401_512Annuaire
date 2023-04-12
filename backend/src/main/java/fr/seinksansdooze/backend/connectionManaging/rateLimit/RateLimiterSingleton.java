cbcdbfadcbaa1ae7f9947fa0bcd0dab0415f8192
fa103555 <antoine.fadda-rodriguez@etu.univ-cotedazur.fr>
2023-04-12 11:04:50 +0200

    [BACK] :bug: nerf du rate limiter qui nous nerf trop

--- backend/src/main/java/fr/seinksansdooze/backend/connectionManaging/rateLimit/RateLimiterSingleton.java
+++ backend/src/main/java/fr/seinksansdooze/backend/connectionManaging/rateLimit/RateLimiterSingleton.java
@@ -2,7 +2,7 @@ package fr.seinksansdooze.backend.connectionManaging.rateLimit;
 
 public enum RateLimiterSingleton {
     INSTANCE;
-    private static final RateLimiter rateLimiter = new RateLimiter(60,60*10);
+    private static final RateLimiter rateLimiter = new RateLimiter(100000,60*10);
 
     public static RateLimiter get() {
         return rateLimiter;
