436b52738e35804b7621a54a17649de3b60a6fb7
ThomasGorisseGit <91067467+ThomasGorisseGit@users.noreply.github.com>
2023-04-13 09:57:44 +0200

    [FRONT] ajout de membre et suppression de membre

--- front/frontAD512Bank/src/app/service/admin.service.ts
+++ front/frontAD512Bank/src/app/service/admin.service.ts
@@ -56,21 +56,27 @@ export class AdminService {
 
   createMember(firstName:string, lastName:string,structureDN:string, title:string, personalPhone:string, professionalPhone:string, dateOfBirth:string, address:string,managerDN:string){
     const options = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-      }),
-      body: {
-        firstName: firstName,
-        lastName: lastName,
-        structureDN: structureDN,
-        title: title,
-        personalPhone: personalPhone,
-        professionalPhone: professionalPhone,
-        dateOfBirth: dateOfBirth,
-        address: address,
-        managerDN: managerDN
-      }
+      firstName: firstName,
+      lastName: lastName,
+      structureDN: structureDN,
+      title: title,
+      personalPhone: personalPhone,
+      professionalPhone: professionalPhone,
+      dateOfBirth: dateOfBirth,
+      address: address,
+      managerDN: managerDN
     }
     return this.http.post(apiURL + "/admin/member/create", options);
+  }
+  removeMemberFromGroup(cn:string, dnUser:string)
+  {
+    return this.http.delete(apiURL + "/admin/group/removeUser",{params:{
+      groupCN:cn,
+      dn:dnUser
+    }})
+  }
+  searchAll()
+  {
+    return this.http.get(apiURL + "/public/search/person", { params: { name: "*", page: 0, perPage: 100 } })
   }
 }
